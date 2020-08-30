d9ed953027dddeaee809b49083e13bd39e94b578
valeriö <valeriodambrosio.14@gmail.com>
2020-08-30 22:44:13 +0200

    getData()

--- Room.php
+++ Room.php
@@ -1,22 +1,32 @@
 <?php
 
-class Room {
 
-  public $id;
-  public $room_number;
-  public $floor;
-  public $beds;
-  public $created_at;
-  public $updated_at;
+  class Room {
 
-  public function __construct($_id, $_room_number, $_floor, $_beds) {
-    $this->id = $_id;
-    $this->room_number = $_room_number;
-    $this->floor = $_floor;
-    $this->beds = $_beds;
+    private $id;
+    protected $room_number;
+    protected $floor;
+    protected $beds;
+    public $created_at;
+    public $updated_at;
+
+    public function __construct($_id, $_room_number, $_floor, $_beds) {
+      $this->id = $_id;
+      $this->room_number = $_room_number;
+      $this->floor = $_floor;
+      $this->beds = $_beds;
+    }
+
+    public function getData() {
+      return [
+        "id" => $this->id,
+        "roomNumber" => $this->room_number,
+        "floor" => $this->floor,
+        "beds" => $this->beds
+      ];
+    }
+
   }
-
-}
 
 
 ?>
--- index.php
+++ index.php
@@ -2,12 +2,24 @@
 require_once(__DIR__ . '/Room.php');
 require_once(__DIR__ . '/db.php');
 
-foreach ($stanze as $stanza) {
+?> <ul class="data" style="display:flex;flex-wrap:wrap;"> <?php
+  foreach ($stanze as $stanza) {
 
-  $iStanza = new Room($stanza["id"], $stanza["room_number"], $stanza["floor"], $stanza["beds"]);
+    $room = new Room(intval($stanza["id"]), intval($stanza["room_number"]), intval($stanza["floor"]), intval($stanza["beds"]));
 
-  var_dump($iStanza);
+    $data = $room->getData();
 
-}
+    ?>
+      <ul class="data_room" style="list-style-type:none;border:1px solid black;padding:10px;">
+        <li> ID: <?php echo $data["id"]; ?> </li>
+        <li> NUMERO: <?php echo $data["roomNumber"]; ?> </li>
+        <li> PIANO: <?php echo $data["floor"]; ?> </li>
+        <li> LETTI: <?php echo $data["beds"]; ?> </li>
+      </ul>
+    <?php
+
+  }
+?> </ul> <?php
+
 
 ?>
